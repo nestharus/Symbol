36c402962729b2851fa7cdd77816733f5142a1e7
Joonas Liik <liik.joonas@gmail.com>
2015-09-01 05:18:34 +0300

    privilege union and test

--- src/main/java/org/gradle/Privilege.java
+++ src/main/java/org/gradle/Privilege.java
@@ -9,12 +9,16 @@ public class Privilege {
 		this.set = ImmutableSet.of(this);
 	}
 	
+	public Privilege(Privilege p1, Privilege p2) {
+		this.set = ImmutableSet.<Privilege>builder().addAll(p1.set).addAll(p2.set).build();
+	}
+	
 	/**
 	 *	Test if the fiven privilege satesfies current privilege
 	 *  ()
 	 */
 	public boolean test(Privilege other){
-		return false;
+		return this.set.containsAll(other.set);
 	}
 
 	public Privilege intersect(Privilege other){
@@ -23,6 +27,6 @@ public class Privilege {
 
 	
 	public Privilege union(Privilege other){
-		return null;
+		return new Privilege(this, other);
 	}
 }
